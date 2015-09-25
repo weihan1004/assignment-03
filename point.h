6ffa0d74f1031994eba932ce4f47f4b9e0944a5d
weihan1004 <1064930456@qq.com>
2015-09-24 20:36:19 -0700

    assignment-03-in-process

---
+++ point.h
@@ -0,0 +1,20 @@
+   /* ----------------------------------------------------------------------------
+   * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+   * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+   * ------------------------------------------------------------------------- */
+
+   /**
+   * A short program to print "Hello World!" to standard output.
+   */
+   
+   #ifndef POINT_H
+   #define POINT_H
+   // ----------------------------------------------------------------------------
+   struct foo {
+   int member;
+   };
+
+   // ... everything besides the copyright information and file description
+
+   // ----------------------------------------------------------------------------
+   #endif  // POINT_H
