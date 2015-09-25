b830f560e8c149c8950a7e2f18d6c044147b9640
weihan1004 <1064930456@qq.com>
2015-09-24 20:32:09 -0700

    Delete point.cpp

--- point.cpp
+++
@@ -1,8 +0,0 @@
-/* ----------------------------------------------------------------------------
-   * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
-   * Released under the [MIT License] (http://opensource.org/licenses/MIT)
-   * ------------------------------------------------------------------------- */
-
-  /**
-   * A short program to print "Hello World!" to standard output.
-   */
