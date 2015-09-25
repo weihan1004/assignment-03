0cf7570f4254bf2520b2aaaa512cadcfe625d0b3
weihan1004 <1064930456@qq.com>
2015-09-24 20:40:16 -0700

    assignment-03-in-process

---
+++ shapes.cpp
@@ -0,0 +1,10 @@
+ /* ----------------------------------------------------------------------------
+   * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+   * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+   * ------------------------------------------------------------------------- */
+
+  /**
+   * A short program to print "Hello World!" to standard output.
+   */
+   
+   virtual bool contains(const Point & p) const = 0;
