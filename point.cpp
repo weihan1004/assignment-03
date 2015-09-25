185a077d04659ac3d2193e0075b2f4f94430c7a5
weihan1004 <1064930456@qq.com>
2015-09-24 20:32:29 -0700

    assignment-03-in-process

---
+++ point.cpp
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+   * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+   * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+   * ------------------------------------------------------------------------- */
+
+  /**
+   * A short program to print "Hello World!" to standard output.
+   */
