e7ef6a84e77c37717b4cafd055230590146da06f
weihan1004 <1064930456@qq.com>
2015-09-24 20:43:11 -0700

    assignment-03-in-process

---
+++ shapes.h
@@ -0,0 +1,17 @@
+ /* ----------------------------------------------------------------------------
+   * Copyright &copy; 2015 Ben Blazak <bblazak@fullerton.edu>
+   * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+   * ------------------------------------------------------------------------- */
+
+  /**
+   * A short program to print "Hello World!" to standard output.
+   */
+   
+   #ifndef SHAPES_H
+   #define SHAPES_H
+   // ----------------------------------------------------------------------------
+   
+   // ... everything besides the copyright information and file description
+   
+   // ----------------------------------------------------------------------------
+   #endif  // SHAPES_H
